80e8819a7f9aa40295696645ecf40968b07a62b3
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-09 13:21:25 -0600

    proxyServer: added Details.
    about to test deployment

--- server.js
+++ server.js
@@ -2,29 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
-});
-
-app.get('/listing/:listingId', (req, res) => {
-  console.log('listingId', req.params.listingId);
-  const id = req.params.listingId;
-  Photos.find({ listingId: id })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../public/index.html`))
 });
 
 const html = path.join(__dirname, '/public/index.html');
 
 app.get('/:listingId', (req, res) => {
   res.sendFile(html);
+});
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
 });
